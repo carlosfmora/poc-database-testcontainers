643464b23db8f25e096382fff4eb565426e99151
fernandomora <carlos.mora@appgate.com>
2024-08-28 20:42:39 -0500

    first commit

---
+++ src/main/java/com/example/pocdatabasetestcontainers/PocDatabaseTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocdatabasetestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDatabaseTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocDatabaseTestcontainersApplication.class, args);
+    }
+
+}
